731da84e44468437f9f94a4b1cfe7549db43e6c8
ushiho <hlotfi.hamza.lotfi@gmail.com>
2018-10-09 20:12:26 +0100

    problem in Mission Form

--- src/Controller/MissionController.php
+++ src/Controller/MissionController.php
@@ -2,18 +2,58 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use DateTime;
+use App\Entity\Mission;
+use App\Form\MissionType;
+use App\Repository\MissionRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MissionController extends AbstractController
 {
     /**
-     * @Route("/mission", name="mission")
+     * @Route("/mission", name="allMissions")
      */
-    public function index()
+    public function index(MissionRepository $repo)
     {
-        return $this->render('mission/index.html.twig', [
-            'controller_name' => 'MissionController',
+        return $this->render('mission/missionBase.html.twig', [
+            'missions' => $repo->findAll(),
+            'connectedUser' => $this->getUser(),
         ]);
+    }
+
+     /**
+     * @Route("/mission/new", name="addMission")
+     * @Route("mission/edit/{id}", name="editMission")
+     */
+    public function create(Request $request, ObjectManager $manager, Mission $mission=null){
+        if($mission==null){
+            $mission = new Mission();
+        }
+        $form = $this->createForm(MissionType::class, $mission);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $mission->setCreatedAt(new \DateTime());
+            $manager->persist($mission);
+            $manager->flush();
+            return $this->redirectToRoute('allMissions');
+        }
+        return $this->render('mission/missionForm.html.twig', [
+            'form' => $form->createView(),
+            'mission' => $mission,
+            'connectedUser' => $this->getUser(),
+
+        ]);
+    }
+
+    /**
+     * @Route("/mission/delete/{id}", name="deleteMission")
+     */
+    public function delete($id, ObjectManager $manager, MissionRepository $repo){
+        $manager->remove($repo->find($id));
+        $manager->flush();
+        return $this->redirectToRoute('allMissions');
     }
 }
--- src/Form/MissionType.php
+++ src/Form/MissionType.php
@@ -4,13 +4,15 @@ namespace App\Form;
 
 use App\Entity\Driver;
 use App\Entity\Mission;
-use App\Entity\Payment;
 use App\Entity\Project;
 use App\Entity\Department;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class MissionType extends AbstractType
 {
@@ -19,10 +21,36 @@ class MissionType extends AbstractType
         $builder
             ->add('startDate', DateType::class)
             ->add('endDate', DateType::class)
-            ->add('driver', Driver::class)
-            ->add('payment', Payment::class)
-            ->add('department', Department::class)
-            ->add('project', Project::class)
+            ->add('driver', EntityType::class, [
+                'class'        => Driver::class,
+                'choice_label' => 'driver',
+                'required'     => true,
+                'placeholder'  => 'choose driver',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('d')
+                        ->orderBy('d.firstName', 'ASC');
+                },
+            ])
+            ->add('department', EntityType::class, [
+                'class' => Department::class,
+                'choice_label' => 'department',
+                'required' => true,
+                'placeholder' => 'choose department',
+                'query_builder' => function (EntityRepository $er){
+                    return $er->createQueryBuilder('d')
+                            ->orderBy('d.name', 'ASC');
+                },
+            ])
+            ->add('project', EntityType::class, [
+                'class' => Project::class,
+                'choice_label' => 'project',
+                'required' => true,
+                'placeholder' => 'link a project',
+                'query_builder' => function (EntityRepository $er){
+                    return $er->createQueryBuilder('p')
+                            ->orderBy('p.name', 'ASC');
+                },
+            ])
         ;
     }
 
